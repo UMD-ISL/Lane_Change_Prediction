2268806ecfca0265d7ea9bbbd76ab55ab4bb066f
mayuan798 <myuan@umich.edu>
2014-10-24 12:52:54 +0800

    temporary updata
    Kirabook has problem again. Almost finish singal preprocessing,
    selection, draw siginal figure part. add mkdir check program to
    deliminate the warning information.

--- signal_selection.m
+++ signal_selection.m
@@ -1,4 +1,4 @@
-function signal_selection(, num_trips, lane_change_labels)
+function signal_selection(num_trips, num_data_columns)
 ini = IniConfig();
 ini.ReadFile('configuration.ini');
 
@@ -6,23 +6,82 @@ ini.ReadFile('configuration.ini');
 Output_Path = ini.GetValues('Signal Selection', 'OUTPUT_PATH');
 home = ini.GetValues('Path Setting', 'HOME_PATH');
 
-time_axis = data_All_cal(:,1)
-
 for m = 1:num_trips
-    mkdir(Output_Path, strcat('figure_', num2str(m)))   % make a folder to store generated images of each vedio
-    load(stract(home, '/Synchronized_Dataset/Vedio_', num2str(m), '_Synchronized_Data.mat');
+    load(strcat(home, '/Synchronized_Dataset/Vedio_', num2str(m), '_Synchronized_Data.mat'));
+    folder_name = strcat(Output_Path, '/vedio_', num2str(m));
+    mkdir_if_not_exist(folder_name);
     
+    time = data_All_cal(:,1);       % first column store the labeled time information
+    target = data_All_cal(:, end);       % last column store the labeled target information
     selected_signal = [];
-    selected_text = [];
+    selected_text = {};
     
-    %% denoise the spikes and choose the signal we want (with interpolate method)
-    for j = 1:(lane_change_labels - 1)      % read the text of each signal and do switch case job
-        signal_text = cell2mat(Text_Index(j+1,1));  % only use the following 'case' signals, use cell2mat function to translate 'cell' to 'mat'
+    % denoise the spikes and choose the signal we want (with interpolate method)
+    for j = 1:(num_data_columns - 1)      % read the text of each signal and do switch case job, ignore the last column (target)
+        signal_type = cell2mat(Text_Index(j+1,1))  % only use the following 'case' signals, use cell2mat function to translate 'cell' to 'mat'
+        if ( strcmp(signal_type, 'Ti/Te') || strcmp(signal_type, 'Ti/Tt') )
+            continue;
+        end
         
-        switch signal_text
+        switch signal_type
             case 'HR'
-                data = data_All_cal(:, j+1);    % extract the j+1 column data, the first column is time
-                selected_signal_text(:, 1) = 'HR';
-                figure_1 = draw_graph(time_axis, selected_signal_text)
+                signal_data = data_All_cal(:, j+1);    % extract the j+1 column data, the first column is time
+                selected_signal (:,1) = signal_data;
+                selected_text{1,1} = signal_type;
+                figure_handle = draw_graph(time, signal_type, signal_data, target, m);
+                saveas(figure_handle, strcat(Output_Path, '/vedio_', num2str(m), '/', signal_type, '_signal_plot'), 'fig');
+                close(figure_handle);
+                continue;
+            case 'Te'
+                signal_data = data_All_cal(:, j+1);    % extract the j+1 column data, the first column is time
+                selected_signal (:,2) = signal_data;
+                selected_text{2,1} = signal_type;
+                figure_handle = draw_graph(time, signal_type, signal_data, target, m);
+                saveas(figure_handle, strcat(Output_Path, '/vedio_', num2str(m), '/', signal_type, '_signal_plot'), 'fig');
+                close(figure_handle);
+                continue;
+            case 'Ti'
+                signal_data = data_All_cal(:, j+1);    % extract the j+1 column data, the first column is time
+                selected_signal (:,2) = signal_data;
+                selected_text{2,1} = signal_type;
+                figure_handle = draw_graph(time, signal_type, signal_data, target, m);
+                saveas(figure_handle, strcat(Output_Path, '/vedio_', num2str(m), '/', signal_type, '_signal_plot'), 'fig');
+                close(figure_handle);
+                continue;
+            case 'Exp Vol'
+                signal_data = data_All_cal(:, j+1);    % extract the j+1 column data, the first column is time
+                selected_signal (:,2) = signal_data;
+                selected_text{2,1} = signal_type;
+                figure_handle = draw_graph(time, signal_type, signal_data, target, m);
+                saveas(figure_handle, strcat(Output_Path, '/vedio_', num2str(m), '/', signal_type, '_signal_plot'), 'fig');
+                close(figure_handle);
+                continue;
+            case 'Insp Vol'
+                signal_data = data_All_cal(:, j+1);    % extract the j+1 column data, the first column is time
+                selected_signal (:,2) = signal_data;
+                selected_text{2,1} = signal_type;
+                figure_handle = draw_graph(time, signal_type, signal_data, target, m);
+                saveas(figure_handle, strcat(Output_Path, '/vedio_', num2str(m), '/', signal_type, '_signal_plot'), 'fig');
+                close(figure_handle);
+                continue;
+            case 'qDEEL'
+                signal_data = data_All_cal(:, j+1);    % extract the j+1 column data, the first column is time
+                selected_signal (:,2) = signal_data;
+                selected_text{2,1} = signal_type;
+                figure_handle = draw_graph(time, signal_type, signal_data, target, m);
+                saveas(figure_handle, strcat(Output_Path, '/vedio_', num2str(m), '/', signal_type, '_signal_plot'), 'fig');
+                close(figure_handle);
+                continue;
+            case 'GSR RAW'
+                signal_data = data_All_cal(:, j+1);    % extract the j+1 column data, the first column is time
+                selected_signal (:,2) = signal_data;
+                selected_text{2,1} = signal_type;
+                figure_handle = draw_graph(time, signal_type, signal_data, target, m);
+                saveas(figure_handle, strcat(Output_Path, '/vedio_', num2str(m), '/', signal_type, '_signal_plot'), 'fig');
+                close(figure_handle);
+                continue;
         end
     end
+    
+    
+end
